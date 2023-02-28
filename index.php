4b390a2366fea9d07129226436b9a8ed36166b0f
Josimar Chagas de Lima <josimareu14@gmail.com>
2023-02-28 10:34:50 -0300

    alteração a index

--- index.php
+++ index.php
@@ -1,44 +1,44 @@
 <?php
-    include_once("templates/header.php");
+include_once("templates/header.php");
 ?>
-    <div class="container">
-        <?php if(isset($printMsg) && $printMsg !=""): ?>
-            <p id="msg"><?=$printMsg?></p>
-        <?php endif; ?>
-            <h1 id="main-title">My Contacts</h1>
-            <?php if(count($contacts)>0): ?>
-                <table class="table" id="contacts-table">
-                    <thead>
+<div class="container">
+    <?php if (isset($printMsg) && $printMsg != "") : ?>
+        <p id="msg"><?= $printMsg ?></p>
+    <?php endif; ?>
+    <h1 id="main-title">My Contacts</h1>
+    <?php if (count($contacts) > 0) : ?>
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($contacts as $contact) : ?>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Telefone</th>
-                        <th scope="col"></th>
+                        <th scope="row"><?= $contact["id"] ?></th>
+                        <td><?= $contact["name"] ?></td>
+                        <td><?= $contact["phone"] ?></td>
+                        <td class="actions">
+                            <a href="<?= $BASE_URL ?>show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
+                            <a href="<?= $BASE_URL ?>edit.php?id=<?= $contact['id'] ?>"><i class="far fa-edit edit-icon"></i></a>
+                            <form class="delete-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+                                <input type="hidden" name="type" value="delete">
+                                <input type="hidden" name="id" value="<?= $contact['id'] ?>">
+                                <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                            </form>
+                        </td>
                     </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($contacts as $contact): ?>
-                        <tr>
-                            <th scope="row"><?= $contact["id"]?></th>
-                            <td><?= $contact["name"]?></td>
-                            <td><?= $contact["phone"]?></td>
-                            <td class="actions">
-                                <a href="<?= $BASE_URL?>show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
-                                <a href="<?= $BASE_URL?>edit.php?id=<?= $contact['id'] ?>"><i class="far fa-edit edit-icon"></i></a>
-                                <form class="delete-form" action="<?= $BASE_URL?>config/process.php" method="POST">
-                                    <input type="hidden" name="type" value="delete">
-                                    <input type="hidden" name="id" value="<?= $contact['id'] ?>">
-                                    <button type="submit"><i class="fas fa-times delete-icon"></i></button>
-                                </form>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
-            <?php else: ?>
-            <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="$BASE_URL?>create.php">Clique aqui para adicionar </a>.</p>
-            <?php endif; ?>
-    </div>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para adicionar </a>.</p>
+    <?php endif; ?>
+</div>
 <?php
-    include_once("templates/footer.php")
+include_once("templates/footer.php")
 ?>
