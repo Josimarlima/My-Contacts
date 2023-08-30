3d78fd444488cee88986e3c08eaef6bf7428996a
Josimar Chagas de Lima <josimareu14@gmail.com>
2023-08-30 16:04:13 -0300

    ajuste do layout

--- index.php
+++ index.php
@@ -11,17 +11,8 @@ include_once("templates/header.php");
 <h1 id="main-title">My Contacts</h1>
 
 
-<!-- Ajusta em ordem crescente e decrescente -->
-<div class="container">
-    <form method="GET" action="index.php" class="mb-3">
-        <label for="order">Ordenar por:</label>
-        <select name="order" id="order">
-            <option value="asc" <?= ($order === 'asc') ? 'selected' : '' ?>>Ordem Crescente</option>
-            <option value="desc" <?= ($order === 'desc') ? 'selected' : '' ?>>Ordem Decrescente</option>
-        </select>
-        <button type="submit" class="btn btn-primary">Aplicar</button>
-    </form>
-    <!-- Botão para abrir o modal -->
+<!-- Botão para abrir o modal -->
+<div class="button-menu-container">
     <div class="search-container">
         <form method="GET" action="#searchResultsModal" class="mb-3">
             <select name="search-type" id="search-type">
@@ -36,41 +27,55 @@ include_once("templates/header.php");
     </div>
 
 
-    <?php if (count($contacts) > 0) : ?>
-        <table class="table" id="contacts-table">
-            <thead>
+    <!-- Ajusta em ordem crescente e decrescente -->
+    <div class="sort-form">
+        <form method="GET" action="index.php" class="mb-3">
+            <label for="order">Ordenar por:</label>
+            <select name="order" id="order">
+                <option value="asc" <?= ($order === 'asc') ? 'selected' : '' ?>>Ordem Crescente</option>
+                <option value="desc" <?= ($order === 'desc') ? 'selected' : '' ?>>Ordem Decrescente</option>
+            </select>
+            <button type="submit" class="btn btn-primary">Aplicar</button>
+        </form>
+    </div>
+</div>
+
+<?php if (count($contacts) > 0) : ?>
+    <table class="table" id="contacts-table">
+        <thead>
+            <tr>
+                <th scope="col" class="col-name">Nome</th>
+                <th scope="col" class="col-lastname">Sobrenome</th>
+                <th scope="col" class="col-phone">Telefone</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php foreach ($contacts as $contact) : ?>
                 <tr>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Sobrenome</th>
-                    <th scope="col">Telefone</th>
-                    <th scope="col"></th>
+                    <td><?= $contact["name"] ?></td>
+                    <td><?= $contact["lastname"] ?></td>
+                    <td><?= $contact["phone"] ?></td>
+                    <td class="actions">
+                        <a href="<?= $BASE_URL ?>show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
+                        <a href="<?= $BASE_URL ?>edit.php?id=<?= $contact['id'] ?>"><i class="far fa-edit edit-icon"></i></a>
+                        <form class="delete-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+                            <input type="hidden" name="type" value="delete">
+                            <input type="hidden" name="id" value="<?= $contact['id'] ?>">
+                            <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                        </form>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($contacts as $contact) : ?>
-                    <tr>
-                        <td><?= $contact["name"] ?></td>
-                        <td><?= $contact["lastname"] ?></td>
-                        <td><?= $contact["phone"] ?></td>
-                        <td class="actions">
-                            <a href="<?= $BASE_URL ?>show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
-                            <a href="<?= $BASE_URL ?>edit.php?id=<?= $contact['id'] ?>"><i class="far fa-edit edit-icon"></i></a>
-                            <form class="delete-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
-                                <input type="hidden" name="type" value="delete">
-                                <input type="hidden" name="id" value="<?= $contact['id'] ?>">
-                                <button type="submit"><i class="fas fa-times delete-icon"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    <?php else : ?>
-        <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para adicionar</a>.</p>
-    <?php endif; ?>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php else : ?>
+    <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para adicionar</a>.</p>
+<?php endif; ?>
 
 
 
-    <?php
-    include_once("templates/footer.php");
-    ?>
+<?php
+include_once("templates/footer.php");
+?>
